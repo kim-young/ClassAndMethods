249ac483e24d8402e265dd98b8ad2f0e4715e07b
kim-young <younghyun87@gmail.com>
2018-05-23 15:26:52 -0400

    project upload

---
+++ ClassProj/Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ClassProj
+{
+    public class Class
+    {
+        public Class()
+        {
+        }
+
+		public static int Addition(int number)
+		{
+			int result = number + number;
+			return result;
+		}
+
+        public static int Subtract(int number)
+		{
+			int result = number - number;
+			return result;
+		}
+
+        public static int Multiply(int number)
+		{
+			int result = number * number;
+			return result;
+		}
+    }
+}
---
+++ ClassProj/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassProj
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+			int number = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(Class.Addition(number));
+			Console.WriteLine(Class.Subtract(number));
+			Console.WriteLine(Class.Multiply(number));
+        }
+    }
+}
